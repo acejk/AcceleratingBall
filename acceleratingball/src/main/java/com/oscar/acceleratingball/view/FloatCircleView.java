92f3d90a1be64ffae5f0ea8ea6aa44a20c31947e
oscar <15737128231@163.com>
2016-08-07 12:31:48 +0800

    浮动小球的绘制

---
+++ acceleratingball/src/main/java/com/oscar/acceleratingball/view/FloatCircleView.java
@@ -0,0 +1,30 @@
+package com.oscar.acceleratingball.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/8/7 0007.
+ */
+public class FloatCircleView extends View {
+    private int mWidth;
+    private int mHeight;
+
+    public FloatCircleView(Context context) {
+        super(context);
+    }
+
+    public FloatCircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FloatCircleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(mWidth, mHeight);
+    }
+}
