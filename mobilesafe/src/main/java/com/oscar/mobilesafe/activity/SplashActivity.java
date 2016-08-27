c90cc71ec03689981f8a94c9a0d94ffbc5940fea
oscar <15737128231@163.com>
2016-08-27 13:01:16 +0800

    引导页

---
+++ mobilesafe/src/main/java/com/oscar/mobilesafe/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.oscar.mobilesafe.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.oscar.mobilesafe.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
